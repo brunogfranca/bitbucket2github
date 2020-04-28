d83cf2324683e30ec7fd93c32caae176dd7a4f3e
Jaie Wilson <jaie.wilson@gmail.com>
2020-04-29 01:19:16 +1000

    minor checks to make sure repo doesn't exist before trying to create it

--- bitbucket2github/github_client.py
+++ bitbucket2github/github_client.py
@@ -1,5 +1,6 @@
 import logging
 from github import Github
+from contextlib import suppress
 
 
 logger = logging.getLogger('bitbucket2github')
@@ -10,21 +11,35 @@ class GitHubClient:
 
     def __init__(self, token, organization, team):
         self.github = Github(token)
-        self.user = self.github.get_user()
         self.organization = organization
         self.team = team
 
     def import_repo(self, repo_data, source_username, source_password):
+        repo_name = repo_data["name"]
         if self.organization is None:
-            logger.info(f'Creating {repo_data["name"]} repository')
-            repo = self.user.create_repo(
+
+            user = self.github.get_user()
+
+            with suppress(Exception):
+                repo_check = user.get_repo(repo_name)
+                if repo_check is not None:
+                    logger.info(f'Repository {repo_name} already exists for user')
+                    return
+
+            logger.info(f'Creating {repo_name} repository')
+            repo = user.create_repo(
                 repo_data['name'],
                 private=repo_data['is_private']
             )
         else:
             # Do org import
-            logger.info(f'Creating {repo_data["name"]} repository in organization {self.organization}')
             org = self.github.get_organization(self.organization)
+
+            with suppress(Exception):
+                repo_check = org.get_repo(repo_name)
+                if repo_check is not None:
+                    logger.info(f'Repository {repo_name} already exists in organization {self.organization}')
+                    return
 
             team_id = None
             if self.team is not None:
@@ -33,13 +48,14 @@ class GitHubClient:
                         team_id = team.id
                         break
 
+            logger.info(f'Creating {repo_name} repository in organization {self.organization}')
             repo = org.create_repo(
                 repo_data['name'],
                 private=repo_data['is_private'],
                 team_id=team_id
             )
 
-        logging.info(f'Migrating {repo_data["name"]} from Bitbucket')
+        logging.info(f'Migrating {repo_name} from Bitbucket')
         repo.create_source_import(
             vcs='git',
             vcs_url=repo_data['url'],
